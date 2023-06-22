31c337b519da3ab5012b9f7ef43d2718d171e09e
guilherme.pnunes <guilherme.pnunes@SBC0505257W10-1>
2023-06-22 11:21:52 -0300

    Adicionar arquivos de projeto.

---
+++ Biblioteca/View/LivrosForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Biblioteca.View
+{
+    partial class LivrosForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbBox_Livro = new System.Windows.Forms.ComboBox();
+            this.txBox_Resul = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome do Livro:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gênero do Livro:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Desconto:";
+            // 
+            // cbBox_Livro
+            // 
+            this.cbBox_Livro.FormattingEnabled = true;
+            this.cbBox_Livro.Location = new System.Drawing.Point(150, 43);
+            this.cbBox_Livro.Name = "cbBox_Livro";
+            this.cbBox_Livro.Size = new System.Drawing.Size(121, 23);
+            this.cbBox_Livro.TabIndex = 4;
+            // 
+            // txBox_Resul
+            // 
+            this.txBox_Resul.BackColor = System.Drawing.Color.Snow;
+            this.txBox_Resul.Location = new System.Drawing.Point(29, 193);
+            this.txBox_Resul.Name = "txBox_Resul";
+            this.txBox_Resul.Size = new System.Drawing.Size(242, 141);
+            this.txBox_Resul.TabIndex = 6;
+            this.txBox_Resul.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(150, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(150, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            // 
+            // LivrosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(296, 357);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txBox_Resul);
+            this.Controls.Add(this.cbBox_Livro);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LivrosForm";
+            this.Text = "LivrosForm";
+            this.Load += new System.EventHandler(this.LivrosForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private ComboBox cbBox_Livro;
+        private Label txBox_Resul;
+        private Label label5;
+        private Label label6;
+    }
+}
